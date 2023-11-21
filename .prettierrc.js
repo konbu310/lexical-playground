389b495a611598477c6eb02e832fbfc8afb0a22b
Yuya Sato <yuyas203@gmail.com>
2023-11-21 09:18:18 +0900

    更新

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  experimentalTernaries: true,
+  printWidth: 80,
+};
+
+module.exports = config;
